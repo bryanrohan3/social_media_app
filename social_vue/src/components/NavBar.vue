fc8b9f8f86d7bbb0e11c3c47230b0db33d042013
Bryan Rohan <bryan.rohan3@mail.dcu.ie>
2024-06-17 15:08:44 +1000

    added <table> instead of <ul> for search on Navbar

--- social_vue/src/components/NavBar.vue
+++ social_vue/src/components/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="navbar-wrapper" :class="{ active: showSearchInput }">
-    <!-- Add an overlay to capture clicks outside the search container -->
     <div
       v-if="showSearchInput"
       class="search-container"
@@ -115,30 +114,28 @@
           @input="debouncedSearch"
           placeholder="Search user profiles..."
         />
-        <ul v-if="searchResults.length">
-          <li
-            v-for="user in searchResults"
-            :key="user.id"
-            @click="goToUserProfile(user.id)"
-          >
-            <!-- Add the @click event handler above -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#000000"
+        <table v-if="searchResults.length">
+          <tbody>
+            <tr
+              v-for="user in searchResults"
+              :key="user.id"
+              @click="goToUserProfile(user.id)"
+              class="user-row"
             >
-              <path
-                d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"
-              />
-            </svg>
-            <div class="username-details">
-              <p class="username">{{ user.username }}</p>
-              <p class="name">{{ user.first_name }} {{ user.last_name }}</p>
-            </div>
-          </li>
-        </ul>
+              <td class="avatar-cell">
+                <img
+                  src="@/assets/avatar.jpeg"
+                  alt="Avatar"
+                  class="avatar-search"
+                />
+              </td>
+              <td class="user-details">
+                <p class="username">{{ user.username }}</p>
+                <p class="name">{{ user.first_name }} {{ user.last_name }}</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </transition>
 
@@ -152,9 +149,9 @@
 <script>
 import { mapGetters, mapMutations } from "vuex";
 import { debounce } from "lodash";
-import axios from "axios";
 import CreatePostModal from "@/components/CreatePostModal.vue";
 import router from "@/router";
+import axiosInstance from "@/api/axiosHelper";
 
 export default {
   data() {
@@ -205,15 +202,9 @@ export default {
 
       try {
         const token = this.userProfile.token;
-        const config = {
-          headers: {
-            Authorization: `Token ${token}`,
-          },
-        };
 
-        const response = await axios.get(
-          `http://127.0.0.1:8000/api/users/?query=${this.searchQuery}`,
-          config
+        const response = await axiosInstance.get(
+          `http://127.0.0.1:8000/api/users/?query=${this.searchQuery}`
         );
         this.searchResults = response.data;
       } catch (error) {
@@ -451,5 +442,58 @@ button:hover {
 .slide-fade-leave-from {
   transform: translateX(0%);
   opacity: 1;
+}
+
+/* table style */
+
+.SearchHeader {
+  font-size: 1.2em;
+  margin-bottom: 10px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.user-row {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+
+.user-row:hover {
+  background-color: #f9f9f9;
+}
+
+.avatar-cell {
+  width: 50px;
+  padding-right: 10px;
+}
+
+.avatar-search {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  border: 1px solid gray;
+}
+
+.user-details {
+  flex: 1;
+}
+
+.username {
+  font-weight: bold;
+  font-size: 0.9em;
+  margin: auto 0;
+}
+
+.name {
+  font-size: 0.8em;
+  color: #555;
+  margin: 0;
+  margin-top: 10px;
 }
 </style>
